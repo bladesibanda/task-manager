d0afd7899669841e3f43346ac5b61c5eb0039378
Blessed Sibanda <bladesibanda@gmail.com>
2023-01-27 14:57:19 +0200

    add persistence

---
+++ src/main/java/com/example/fullstack/task/Task.java
@@ -0,0 +1,39 @@
+package com.example.fullstack.task;
+
+import java.time.ZonedDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Version;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.example.fullstack.user.User;
+
+import io.quarkus.hibernate.reactive.panache.PanacheEntity;
+
+@Entity
+@Table(name = "tasks")
+public class Task extends PanacheEntity {
+  @Column(nullable = false)
+  public String title;
+
+  @Column(length = 1000)
+  public String description;
+
+  public Integer priority;
+
+  @ManyToOne(optional = false)
+  public User user;
+
+  public ZonedDateTime complete;
+
+  @CreationTimestamp
+  @Column(updatable = false, nullable = true)
+  public ZonedDateTime created;
+
+  @Version
+  public int version;
+}
